74285e7e164dd68f0a155a7314c70927734ef930
113062301 <kawahehehaha@gapp.nthu.edu.tw>
2026-05-25 17:08:15 +0800

    hud

--- assets/Script/UI/GameUI.ts
+++ assets/Script/UI/GameUI.ts
@@ -4,16 +4,17 @@ import GameManager from '../Managers/GameManager';
 @ccclass
 export default class GameUI extends cc.Component {
 
-    @property(cc.Label)
-    livesLabel: cc.Label = null;
+    @property(cc.Label) livesLabel: cc.Label = null;
+    @property(cc.Label) scoreLabel: cc.Label = null;
+    @property(cc.Label) timerLabel: cc.Label = null;
 
-    @property(cc.Label)
-    scoreLabel: cc.Label = null;
+    @property timeLimit: number = 180;
 
-    @property([cc.Node])
-    lifeIcons: cc.Node[] = [];
+    private _timeLeft: number = 0;
+    private _ticking: boolean = true;
 
     onLoad() {
+        this._timeLeft = this.timeLimit;
         this.refreshUI();
         if (GameManager.inst) {
             GameManager.inst.onLivesChanged = () => this.refreshUI();
@@ -21,13 +22,31 @@ export default class GameUI extends cc.Component {
         }
     }
 
+    update(dt: number) {
+        if (!this._ticking) return;
+        this._timeLeft -= dt;
+        if (this._timeLeft <= 0) {
+            this._timeLeft = 0;
+            this._ticking = false;
+            GameManager.inst?.loseLife();
+        }
+        if (this.timerLabel) {
+            // 只顯示數字，不顯示 TIME
+            this.timerLabel.string = Math.ceil(this._timeLeft).toString().padStart(3, '0');
+            this.timerLabel.node.color = this._timeLeft <= 30
+                ? cc.color(255, 60, 60)
+                : cc.color(255, 255, 255);
+        }
+    }
+
     refreshUI() {
         const gm = GameManager.inst;
         if (!gm) return;
-        if (this.livesLabel) this.livesLabel.string = '× ' + gm.lives;
+        if (this.livesLabel) this.livesLabel.string = String(gm.lives);
         if (this.scoreLabel) this.scoreLabel.string = String(gm.score).padStart(6, '0');
-        this.lifeIcons.forEach((icon, i) => { if (icon) icon.active = i < gm.lives; });
     }
+
+    stopTimer() { this._ticking = false; }
 
     onDestroy() {
         if (GameManager.inst) {
